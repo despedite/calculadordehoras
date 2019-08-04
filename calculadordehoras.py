d6472b57ac4f690c9669227f90314077bd08bc03
Erik Bianco Vera <36340014+despediteerik@users.noreply.github.com>
2019-08-04 13:04:37 -0300

    now sum of all hours works! also changes visually

--- calculadordehoras.py
+++ calculadordehoras.py
@@ -1,15 +1,19 @@
 import pyfiglet
+import os
 
 banner = pyfiglet.figlet_format("Calculador de Horas")
 
 pagaporhora = 80
 
+os.system('cls')
 print(banner)
 print("Introduce la cantidad de rondas:")
 cantidaditerable = int(input())
 calraw = [None] * cantidaditerable
+os.system('cls')
+print(banner)
 for x in range(cantidaditerable):
-	print("\nIntroduce la hora de entrada. (formato: XX:YY)")
+	print("\n--- Ronda " + str(x+1) + " ---\nIntroduce la hora de entrada. (formato: XX:YY)")
 	entrada = input()
 	defentrada = entrada.split(':')
 	horaentrada = int(defentrada[0])
@@ -27,11 +31,18 @@ for x in range(cantidaditerable):
 	calminutos = minutosalida - minutoentrada
 	calraw[x] = (calhoras*60) + calminutos
 	if x > 0:
-		calacumulado = calraw[x] + calraw[x-1]
-		print ("Trabajaste " + str(calhoras) + ":" + str(calminutos) + " (" + str(calraw[x]) + " minutos).")
+		calacumulado = calraw[x] + calacumulado
+		if calminutos < 10:
+			print ("Trabajaste " + str(calhoras) + ":0" + str(calminutos) + " (" + str(calraw[x]) + " minutos).")
+		else:
+			print ("Trabajaste " + str(calhoras) + ":" + str(calminutos) + " (" + str(calraw[x]) + " minutos).")
 		print("Eso es un total de "+str(calacumulado)+" minutos!")
 	else:
-		print ("Trabajaste " + str(calhoras) + ":" + str(calminutos) + " (" + str(calraw[x]) + " minutos).")
+		calacumulado = calraw[x]
+		if calminutos < 10:
+			print ("Trabajaste " + str(calhoras) + ":0" + str(calminutos) + " (" + str(calraw[x]) + " minutos).")
+		else:
+			print ("Trabajaste " + str(calhoras) + ":" + str(calminutos) + " (" + str(calraw[x]) + " minutos).")
 
 pagaporminuto = pagaporhora / 60
 cuantaplata = calacumulado * pagaporminuto
@@ -40,6 +51,9 @@ horastotales = calacumulado / 60
 minutostemp = horastotales % 1
 minutostotales = minutostemp * 60
 
-print("\nTrabajaste un gran total de " + str(int(horastotales)) + ":" + str(int(
-minutostotales)) +  " (" + str(calacumulado) + " minutos)")
+if minutostotales < 10:
+	print("\nTrabajaste un gran total de " + str(int(horastotales)) + ":0" + str(int(minutostotales)) +  " (" + str(calacumulado) + " minutos)")
+else:
+	print("\nTrabajaste un gran total de " + str(int(horastotales)) + ":" + str(int(minutostotales)) +  " (" + str(calacumulado) + " minutos)")
 print("¡Eso te da un gran total de " + str(round(cuantaplata)) + "$, a " + str(round(pagaporhora)) + " pesos la hora!")
+input('\n--- ENTER para salir ---')
